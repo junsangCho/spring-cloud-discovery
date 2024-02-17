e607b36a57f6f2fea86499e0d9c6a064d73bed12
junsangCho <wnstkddl90@naver.com>
2024-02-17 19:07:45 +0900

    Feat: 로드밸런싱 통신 테스트 프로젝트 추가  - discovery-client2, discovery-client3 프로젝트 추가  - Openfign 을 이용한 로드밸런싱 테스트코드 추가  - 각 프로젝트별 README 추가

---
+++ discovery-client3/src/test/java/springcloud/discoveryclient3/DiscoveryClient3ApplicationTests.java
@@ -0,0 +1,13 @@
+package springcloud.discoveryclient3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryClient3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
